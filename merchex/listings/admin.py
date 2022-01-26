f46f691f9a0872b17c874e077dd6594209324b71
Leo <leo.gabaix@viacesi.fr>
2022-01-26 22:00:27 +0100

    updated administrated model

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from listings.models import Band, Listing
 # Register your models here.
+
+
+class BandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'gender', 'year_formed')
+
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'type', 'year', 'sold')
+
+
+admin.site.register(Band, BandAdmin)
+admin.site.register(Listing, ListingAdmin)
--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -3,6 +3,9 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Band(models.Model):
+    def __str__(self) -> str:
+        return f'{self.name}'
+
     class Gender(models.TextChoices):
         HIP_HOP = 'HH'
         SYNTH_POP = 'SP'
